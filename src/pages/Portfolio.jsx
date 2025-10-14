e72b168dd11e3718aa4bfdcb9421bcd8ed63f742
John Doe <antorremeni@gmail.com>
2025-10-14 18:13:11 +0600

    chnage

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -226,7 +226,7 @@ return (
      data-aos-easing="ease-in-sine" className="bg-[#161616] h-[280px] px-[20px] pt-[20px] max-md:w-full max-md:h-auto max-md:pb-[20px]">
     <h2 className="text-[21px] font-medium font-main text-[#fff]">Js-Whether</h2>
     <div className="w-[500px] pt-[20px] pb-[50px] max-md:w-full max-md:pb-[20px]">
-    <p className="text-[16px] font-normal font-main text-[#9F9F9F]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, doloremque aperiam placeat vitae ab suscipit labore consectetur natus soluta hic quo, nesciunt saepe praesentium corporis doloribus adipisci commodi harum?</p> </div>
+    <p className="text-[16px] font-normal font-main text-[#9F9F9F]"> this is javascript api fatch project where api fatch in anycronus  </p> </div>
 
     <div className="w-[100px] flex justify-center py-[10px] px-[35px] hover:text-[#009e66] duration-[.4s] hover:bg-transparent text-base font-semibold font-main text-[#fff] bg-[#009e66] border-[#009e66] rounded-full border max-md:mx-auto">
       <Link to={'/'} >View</Link>

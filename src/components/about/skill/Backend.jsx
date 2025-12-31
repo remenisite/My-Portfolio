855ae52bba9081e63eedad1fced27088c95206f0
antor94 <antorremeni@gmail.com>
2025-12-31 19:51:42 +0600

    change

---
+++ src/components/about/skill/Backend.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Backend = () => {
+  return (
+    <>
+    
+    
+    
+     {/* ---------------- backend skill */}
+                <div className="w-[500px] mt-[400px] lg:mt-[50px]   gap-[30px] ">
+                  <div
+                    data-aos="fade-left"
+                    className=" flex justify-center items-center gap-[30px] p-[30px] bg-[#161616]"
+                  >
+                    <div className=" pr-[136px] lg:pr-0 flex flex-col justify-center items-center">
+                      <h2 className="text-[36px] font-bold font-main text-white">
+                        Learning
+                      </h2>
+                      <h2 className=" text-[20px] font-medium font-main text-[#F0F0F0] pb-[5px]">
+                        Backend Development with Node.js
+                      </h2>
+                    </div>
+                  </div>
+                </div>
+    
+    
+    
+    
+    
+    </>
+  )
+}
+
+export default Backend
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -63,22 +63,7 @@ const About = () => {
               <Frontend />
 
               <div className="flex justify-center gap-[100px]">
-                {/* ---------------- backend skill */}
-                <div className="w-[500px] mt-[400px] lg:mt-[50px]   gap-[30px] ">
-                  <div
-                    data-aos="fade-left"
-                    className=" flex justify-center items-center gap-[30px] p-[30px] bg-[#161616]"
-                  >
-                    <div className=" pr-[136px] lg:pr-0 flex flex-col justify-center items-center">
-                      <h2 className="text-[36px] font-bold font-main text-white">
-                        Learning
-                      </h2>
-                      <h2 className=" text-[20px] font-medium font-main text-[#F0F0F0] pb-[5px]">
-                        Backend Development with Node.js
-                      </h2>
-                    </div>
-                  </div>
-                </div>
+               
               </div>
             </div>
             {/* ------------- service-part */}

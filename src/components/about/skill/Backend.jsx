c32e6f593392925741cded20b85f95cee5d5571e
antor94 <antorremeni@gmail.com>
2026-01-16 19:08:54 +0600

    change

--- src/components/about/skill/Backend.jsx
+++ src/components/about/skill/Backend.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 
-import _back1  from '../../../assets/images/back1.png'
-import _back2  from '../../../assets/images/back2.png'
-import _back3  from '../../../assets/images/back3.png'
-import _back4  from '../../../assets/images/back4.png'
-import _back5  from '../../../assets/images/back5.png'
-
-
+import _back1 from "../../../assets/images/back1.png";
+import _back2 from "../../../assets/images/back2.png";
+import _back3 from "../../../assets/images/back3.png";
+import _back4 from "../../../assets/images/back4.png";
+import _back5 from "../../../assets/images/back5.png";
 
 const Backend = () => {
   return (
@@ -15,27 +13,23 @@ const Backend = () => {
       <div className="w-[500px] mt-[400px] lg:mt-[50px]   gap-[30px] ">
         <div
           data-aos="fade-left"
-          className=" flex justify-center items-center gap-[30px] p-[30px] bg-[#161616]" >
-
-                 <div data-aos="flip-down" className="text-center ">
-                        <img className="w-[50px] text-center" src={_back1} alt="logo" />
-                      </div>
-                           <div data-aos="flip-down" className="text-center ">
-                                  <img className="w-[50px] text-center" src={_back2} alt="logo" />
-                                </div>
-                                
-
-
-
-
-          {/* <div className=" pr-[136px] lg:pr-0 flex flex-col justify-center items-center">
-            <h2 className="text-[36px] font-bold font-main text-white">
-              Learning
-            </h2>
-            <h2 className=" text-[20px] font-medium font-main text-[#F0F0F0] pb-[5px]">
-              Backend Development with Node.js
-            </h2>
-          </div> */}
+          className=" flex justify-center items-center gap-[30px] p-[30px] bg-[#161616]"
+        >
+          <div data-aos="flip-down" className="text-center ">
+            <img className="w-[50px] text-center" src={_back1} alt="logo" />
+          </div>
+          <div data-aos="flip-down" className="text-center ">
+            <img className="w-[50px] text-center" src={_back2} alt="logo" />
+          </div>
+          <div data-aos="flip-down" className="text-center ">
+            <img className="w-[50px] text-center" src={_back3} alt="logo" />
+          </div>
+          <div data-aos="flip-down" className="text-center ">
+            <img className="w-[50px] text-center" src={_back4} alt="logo" />
+          </div>
+          <div data-aos="flip-down" className="text-center ">
+            <img className="w-[50px] text-center" src={_back5} alt="logo" />
+          </div>
 
 
         </div>
